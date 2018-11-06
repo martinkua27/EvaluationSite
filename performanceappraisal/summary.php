64a39690ad941065cbe665006b27fe877ffd4867
Kuzikume <ezekiel.cruz666@gmail.com>
2018-11-06 15:20:49 +0800

    COMMITED
    EYYY

--- performanceappraisal/summary.php
+++ performanceappraisal/summary.php
@@ -87,6 +87,7 @@ include('../session.php');
 	}
 	
 	</style>
+	
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/1.4.1/jspdf.debug.js" integrity="sha384-THVO/sM0mFD9h7dfSndI6TS0PgAGavwKvB5hAxRRvc0o9cPLohB0wb/PTA7LdUHs" crossorigin="anonymous"></script>
 
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
