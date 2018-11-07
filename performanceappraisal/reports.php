fbf3df310d59e09c4e6fb5c030f6885542bf363c
Martin Angelo Kua <mak942708@gmail.com>
2018-11-08 00:07:02 +0800

    Position added to classroom observation

--- performanceappraisal/reports.php
+++ performanceappraisal/reports.php
@@ -365,6 +365,7 @@ include('../session.php');
                     <thead class="thead-dark">                            
                        <tr>
                          <td>Evaluator</td>
+                         <td>Postion</td>
                          <td>Employee Evaluated</td>
                          <td>A Average</td>
                          <td>B Average</td>
@@ -390,6 +391,7 @@ include('../session.php');
                     ?>
                      <tr>
                          <td data-label="Evaluator"><?php echo $row['evaluator']?></td>
+                         <td data-label="Position"><?php echo $row['position']?></td>
                          <td data-label="Employee Evaluated"><?php echo $row['emp_name_evaluated']?></td>
                          <td data-label="A Average"><?php echo $row['a_average']?></td>
                          <td data-label="B Average"><?php echo $row['b_average']?></td>
