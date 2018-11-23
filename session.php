e95a6541352b310b25d69d345315ba5471d9d02f
Martin Angelo Kua <mak942708@gmail.com>
2018-11-24 03:01:58 +0800

    Update session.php

--- session.php
+++ session.php
@@ -3,13 +3,14 @@
    session_start();
    include('indexDB.php');
    error_reporting(E_ALL ^ E_NOTICE);
+
    
    $user_check = $_SESSION['login_user'];
    $userStat = $_SESSION['login_userStat'];
    $user_pass_check = $_SESSION['login_pass'];
 
 
-
+   
    
  
       $conn = new mysqli($servername, $username, $password, $dbname);
@@ -20,18 +21,19 @@
          
           
                  $login_session = $row['emp_id'];
-
+                
         
     }
     
     
-    // output data of each r1ow
-    
+ 
     
    
    if(!isset($_SESSION['login_user'])){
     
         echo "<script>alert('Please Login');document.location='index.php'</script>";
-   }
+    }
+
+
          
 ?>
